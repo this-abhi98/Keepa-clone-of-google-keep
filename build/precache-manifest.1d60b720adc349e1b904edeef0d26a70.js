4051526a6c87dd2e42c37a5899cbcfa0c9a475b3
abhishek <this.abhi98@gmail.com>
2020-06-29 03:40:49 +0530

    second commit

---
+++ build/precache-manifest.1d60b720adc349e1b904edeef0d26a70.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8779e2776afeeeb5d39ce2b36d868e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "316afcb9dd5c51ee062c",
+    "url": "/static/js/2.62ad27e4.chunk.js"
+  },
+  {
+    "revision": "ed33391bab53fc70e3c4",
+    "url": "/static/js/main.86ea5491.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
